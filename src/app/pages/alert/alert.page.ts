605722be7847c99cd86715210e90eff5007b0230
= <=>
2022-08-13 12:58:41 -0400

    Primer Commit

---
+++ src/app/pages/alert/alert.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.page.html',
+  styleUrls: ['./alert.page.scss'],
+})
+export class AlertPage implements OnInit {
+
+  title:string = 'alerta';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
